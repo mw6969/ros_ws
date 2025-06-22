d0bc36f5e95fc7daab3680bc338e166fb360c777
Dmytro Malysh <d.malysh123@gmail.com>
2025-06-22 15:20:45 +0200

    Initial commit of ROS workspace.

---
+++ src/python_bindings_tutorial/src/python_bindings_tutorial/_add_two_ints_wrapper_py.py
@@ -0,0 +1,55 @@
+from io import BytesIO
+
+import rospy
+from std_msgs.msg import Int64
+
+from python_bindings_tutorial._add_two_ints_wrapper_cpp import AddTwoIntsWrapper
+
+
+class AddTwoInts(object):
+    def __init__(self):
+        self._add_two_ints = AddTwoIntsWrapper()
+
+    def _to_cpp(self, msg):
+        """Return a serialized string from a ROS message
+
+        Parameters
+        ----------
+        - msg: a ROS message instance.
+        """
+        buf = BytesIO()
+        msg.serialize(buf)
+        return buf.getvalue()
+
+    def _from_cpp(self, str_msg, cls):
+        """Return a ROS message from a serialized string
+
+        Parameters
+        ----------
+        - str_msg: str, serialized message
+        - cls: ROS message class, e.g. sensor_msgs.msg.LaserScan.
+        """
+        msg = cls()
+        if isinstance(str_msg, str):
+            str_msg = str_msg.encode('latin1')
+        msg.deserialize(str_msg)
+        return msg
+
+    def add(self, a, b):
+        """Add two std_mgs/Int64 messages
+
+        Return a std_msgs/Int64 instance.
+
+        Parameters
+        ----------
+        - a: a std_msgs/Int64 instance.
+        - b: a std_msgs/Int64 instance.
+        """
+        if not isinstance(a, Int64):
+            rospy.ROSException('Argument 1 is not a std_msgs/Int64')
+        if not isinstance(b, Int64):
+            rospy.ROSException('Argument 2 is not a std_msgs/Int64')
+        str_a = self._to_cpp(a)
+        str_b = self._to_cpp(b)
+        str_sum = self._add_two_ints.add(str_a, str_b)
+        return self._from_cpp(str_sum, Int64)
